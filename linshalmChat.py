38df999516d1b79a87094a8dbdd3432cd0103a10
janiokj <janiokj@gmail.com>
2024-07-12 17:17:09 -0300

    Fim do Projeto

--- linshalmChat.py
+++ linshalmChat.py
@@ -15,29 +15,31 @@ def main(pagina): #def cria uma função, no caso de site ou App obrigatoriament
     titulo_janela = ft.Text("Seja Bem Vindo ao Chat Linshalm")
     campo_nome_usuario = ft.TextField(label="Escreva seu nome no Chat")
 
-    texto_mensagem = ft.TextField(label="Digite a sua mensagem:")
+    
+
+    
 
     #socket = tunel de comunicação - sempre que quiser que algo aconteça para mais do que um usuário simultaneamente
     def enviar_mensagem_tunel(mensagem):
-        print(mensagem)
-
-    pagina.pubsub.subscribe(enviar_mensagem_tunel) #Comando para CRIAR o socket/Tunel de comunicação
+        chat.controls.append(ft.Text(mensagem))
+        pagina.update() 
     
+    pagina.pubsub.subscribe(enviar_mensagem_tunel) #Comando para CRIAR o socket/Tunel de comunicação
 
     #Funcao para Enviar a Mensagem
     def enviar_mensagem(evento):
         #enviar a mensagem do usuário
         texto = f"{campo_nome_usuario.value}: {texto_mensagem.value}"
-        chat.controls.append(ft.Text(texto))
+        
 
         #Enviar uma mensagem no Tunel
-        pagina.pubsub.send_all("É Nóis...")   
+        pagina.pubsub.send_all(texto)   
 
         #limpar o campo de mensagem
         texto_mensagem.value = ""
         pagina.update()
 
-
+    texto_mensagem = ft.TextField(label="Digite a sua mensagem:",on_submit=enviar_mensagem) #on_sumit - enviar mensagem
     botao_enviar = ft.ElevatedButton("Enviar",on_click=enviar_mensagem)
 
     #Criar CHAT
@@ -61,7 +63,8 @@ def main(pagina): #def cria uma função, no caso de site ou App obrigatoriament
 
         #Escrever: Usuario entrou no site
         texto_entrou_chat = f"{campo_nome_usuario.value} entrou no Chat"
-        chat.controls.append(ft.Text(texto_entrou_chat)) #Append: Adicionar um item a lista
+        #chat.controls.append(ft.Text(texto_entrou_chat)) #Append: Adicionar um item a lista
+        pagina.pubsub.send_all(texto_entrou_chat)
 
         pagina.update()
 
@@ -88,5 +91,5 @@ def main(pagina): #def cria uma função, no caso de site ou App obrigatoriament
 
 
 #Executar o Sistema
-#ft.app(main, view=ft.WEB_BROWSER) #Esse cara defini também se a saída(view) vai ser Um navegador(WEB_BROWSER), um app ou um programa
-ft.app(main)
+ft.app(main, view=ft.WEB_BROWSER) #Esse cara defini também se a saída(view) vai ser Um navegador(WEB_BROWSER), um app ou um programa
+#ft.app(main)
